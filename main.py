3f37a3990632c35b2c45a4966fd3fcfb258a807b
hwahyeon <hwahyeon@knou.ac.kr>
2023-12-30 17:18:37 +0900

    Classfied Hard Mode and All Mode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import config
 from preprocess.preprocess_easy_dataset import process as preprocess_easy
 from preprocess.preprocess_imdt_dataset import process as preprocess_imdt
 from preprocess.preprocess_hard_dataset import process as preprocess_hard
+from preprocess.prepocess_all_dataset import process as preprocess_all
 
 def preprocess_all_datasets():
     # dataset1 폴더의 파일들을 전처리
@@ -12,6 +13,9 @@ def preprocess_all_datasets():
 
     # dataset3 폴더의 파일들을 전처리
     preprocess_hard(config.DATASET3_PATH, config.HARD_MODE_OUTPUT_PATH)
+
+    # dataset3 폴더의 파일들을 전처리 (모든 의미를 포함한 사전 형식으로)
+    preprocess_all(config.DATASET3_PATH, config.ALL_MODE_OUTPUT_PATH)
 
 if __name__ == "__main__":
     preprocess_all_datasets()
--- preprocess/preprocess_hard_dataset.py
+++ preprocess/preprocess_hard_dataset.py
@@ -1,48 +1,39 @@
 from preprocess.common_preprocessing import hangul_decompose, save_json
 import pandas as pd
 import os
-from collections import defaultdict
-
-def extract_data(file_path):
+def filter_data(file_path):
     data = pd.read_excel(file_path)
-
     condition_pos = data['품사'].isin(['명사', '감·명', '관·명', '명·부', '의존 명사'])
     condition_unit = data['구성 단위'] == '단어'
     condition_category = data['범주'].isna()
+    return data[condition_pos & condition_unit & condition_category]
 
-    filtered_data = data[condition_pos & condition_unit & condition_category]
+def process_words(filtered_data):
+    result_set = set()
+    first_column = filtered_data.iloc[:, 0].str.replace('-', '')
+    
+    # 단어 길이 3개 이하만 추리기 (보수적으로 잡음)
+    short_words = first_column[first_column.str.len() <= 3]
+    result_set.update(short_words)
 
-    # 필요한 열 추출
-    result = filtered_data[['어휘', '뜻풀이']]
-    return result
-
-def process_words(data):
-    data['어휘'] = data['어휘'].str.replace('-', '')
-
-    # 각 어휘에 대한 모든 뜻풀이와 분해된 단어를 저장할 defaultdict 생성
-    word_info = defaultdict(lambda: {'meanings': [], 'decomposed': ''})
-
-    for index, row in data.iterrows():
-        decomposed_word = hangul_decompose(row['어휘'])
+    wordle_list = []
+    for word in result_set:
+        decomposed_word = hangul_decompose(word)
         if len(decomposed_word) == 5:
-            word_info[row['어휘']]['meanings'].append(row['뜻풀이'])
-            word_info[row['어휘']]['decomposed'] = decomposed_word
-
-    # defaultdict에서 딕셔너리 리스트로 변환
-    word_list = [
-        {'key': word, 'value': info['decomposed'], 'mean': mean}
-        for word, info in word_info.items() for mean in info['meanings']
-    ]
-
-    return word_list
+            wordle_list.append(decomposed_word)
+            decomposed_data = {
+                'key': word,
+                'value': decomposed_word
+            }
+            wordle_list.append(decomposed_data)
+    return wordle_list
 
 def process_files(file_path, output_path):
-    filtered_data = extract_data(file_path)
+    filtered_data = filter_data(file_path)
     wordle_list = process_words(filtered_data)
     save_json(wordle_list, output_path, append=True)
 
 def process(directory_path, output_path):
-    print('hey')
     for filename in os.listdir(directory_path):
         if filename.endswith('.xls') or filename.endswith('.xlsx'):
             process_files(os.path.join(directory_path, filename), output_path)
